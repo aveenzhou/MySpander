fa77a2d2193d900d4aaf8f959a0d2fe49854147d
chow <aveenzhou@gmail.com>
2017-04-11 14:37:40 +0800

    配置文件支持URL列表，支持批量下载

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Created on 2015-9-25
 
 @author: Administrator
 '''
+import re
 from ConfigParser import ConfigParser
 from download import down_pics_to_folder
 from joinpics import join_pics
@@ -13,25 +14,62 @@ from pastelogo import pasteLogo
 cf=ConfigParser()
 cf.read('config.ini')
 
-source_url = cf.get('source','url')
-print source_url
-store_dir  = cf.get('store','dir')
-print store_dir
-logo_path = cf.get('logo','path')
-print logo_path
-logo_x=cf.get('logo','x')
-logo_y=cf.get('logo','y')
-mainflag = int(cf.get('flag','mainPic'))
+reg_lst=re.compile(r"^\[([\s\S]+)\]$")
+re_int =re.compile(r"^(\d+)$")
+re_float = re.compile(r"^(\d+\.\d+)$")
+
+re_w=re.compile(r"\s")
+ 
 
 
-if __name__=='__main__':
+ 
+def getIni(field,key):
+    data = cf.get(field,key)
+    if re_int.match(data):
+        return int(re_int.match(data).group(1))
+    if re_float.match(data):
+        return int(re_float.match(data).group(1))
+    if reg_lst.match(data):
+        try:
+            return re_w.sub("",reg_lst.match(data).group(1)).split(",")
+        except Exception,e:
+            return "data type error"
     
+    return data
+
+
+source_url = getIni('source','url')
+print source_url
+store_dir  = getIni('store','dir')
+print store_dir
+logo_path = getIni('logo','path')
+print logo_path
+logo_x=getIni('logo','x')
+logo_y=getIni('logo','y')
+mainflag = getIni('flag','mainPic')
+
+
+def getData(source_url,mainflag,store_dir):
     res=down_pics_to_folder(source_url,mainflag,store_dir)
     if res:
         join_pic_dir,store_dir=res
         if not mainflag:
             join_pics(join_pic_dir,store_dir)
         pasteLogo(store_dir,logo_path,(int(logo_x),int(logo_y)))
+
+
+
+
+if __name__=='__main__':
+    
+    if type(source_url) is list:
+        for url in source_url:
+            getData(url,mainflag,store_dir)
+    else:
+        getData(source_url,mainflag,store_dir)
+    
+    
+
 
 
 
